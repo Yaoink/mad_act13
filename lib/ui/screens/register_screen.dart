e6681ceb4bbd625159a968ba39b3ad093b78a7b4
Nancy Quan <51468095+Yaoink@users.noreply.github.com>
2026-04-09 12:06:01 -0400

    feat/connect form to auth service

--- lib/ui/screens/register_screen.dart
+++ lib/ui/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:firebase_auth/firebase_auth.dart";
+import "/services/auth_service.dart";
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -9,6 +10,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final AuthService _authService = AuthService();
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -24,7 +26,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Register"),
+        title: const Text("Sign Up/In"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -33,16 +35,17 @@ class _RegisterScreenState extends State<RegisterScreen> {
           children: [
             TextFormField(
               controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
+              decoration: const InputDecoration(labelText: 'Email', prefixIcon: Icon(Icons.lock)),
               validator: (value) {
                   if (value == null || value.isEmpty) return 'Email is required';
                   if (!value.contains('@')) return 'Enter a valid email like test@gsu.com';
                   return null;
               },
             ),
+            const SizedBox(height: 20),
             TextFormField(
                 controller: _passwordController,
-                decoration: const InputDecoration(labelText: 'Password'),
+                decoration: const InputDecoration(labelText: 'Password', prefixIcon: Icon(Icons.lock)),
                 obscureText: true,
                 validator: (value) {
                     if (value == null || value.length < 6) return 'Password must be at least 6 characters';
@@ -50,26 +53,46 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 },
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  try {
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                      email: _emailController.text,
-                      password: _passwordController.text,
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Registration successful!')),
-                    );
-                    Navigator.pop(context);
-                  } on FirebaseAuthException catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Error: ${e.message}')),
-                    );
-                  }
-                }
-              },
-              child: const Text('Register'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                        try {
+                          await _authService.register(_emailController.text, _passwordController.text);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Registration successful!')),
+                          );
+                          Navigator.pop(context);
+                        } on FirebaseAuthException catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Error: ${e.message}')),
+                          );
+                        }
+                      }
+                    },
+                  child: const Text('Register'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      try {
+                        await _authService.signIn(_emailController.text, _passwordController.text);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Sign in successful!')),
+                        );
+                        Navigator.pop(context);
+                      } on FirebaseAuthException catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Error: ${e.message}')),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text('Sign In'),
+                ),
+              ],
             ),
           ],
         ),
